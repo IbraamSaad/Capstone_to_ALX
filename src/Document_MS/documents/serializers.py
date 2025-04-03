ef86aaeb090acc569b9685e9bb887ea733104260
IbraamSaad <ebramb37@gmail.com>
2025-04-03 02:43:03 +0200

    crearing serializers.py file for conversion data and handling aAPIs

---
+++ src/Document_MS/documents/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from . models import CustomUser, ProjectName, Documents
+
+class SerialzerCustomeUser(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+
+class SerializerProjectName(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectName
+        fields = '__all__'
+
+class SerializerDocuments(serializers.ModelSerializer):
+    class Meta:
+        model = Documents
+        fields = '__all__'
